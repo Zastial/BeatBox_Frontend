e50dd2d82452f12a4c1929fe4a0cfe10f772247a
Zastial <sasandre10@gmail.com>
2024-11-28 10:57:51 +0000

    Update vite-config to handle WSS for WebSocket connexion (when calling backend API from HTTPS)

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+        hmr: {
+            host: "beatbox.sunlamander.tech",
+            protocol: "wss",
+	    port: 433,
+        },
+    },
 })
